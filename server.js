859aa02adf4de2bffdacec0a8276a7e69e4554e8
Hena-yaris <meraey21@gmail.com>
2024-02-18 15:05:51 -0800

    the server is listening

--- server.js
+++ server.js
@@ -2,3 +2,11 @@
 const express= require('express')
 const app=express();
 
+const server=require('http').Server(app);
+
+app.get("/",(req,res) => {
+    res.end("hi hena i comming")
+})
+
+
+server.listen(process.env.PORT || 3000);
