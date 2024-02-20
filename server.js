8e4dff14a9229a983ebaaefa458579d85059c53f
Hena-yaris <meraey21@gmail.com>
2024-02-20 08:03:05 -0800

    webrtc peerjs

--- server.js
+++ server.js
@@ -1,35 +1,40 @@
 const express = require("express");
 const app = express();
-
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 const { v4: uuidV4 } = require("uuid");
 
+app.use("/peerjs", peerServer);
 
-//permission
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-
-
-io.on("connection", (socket) => {
-  socket.on("join-room", ()=>{
-    console.log("habtsh joined");
-  });
-});
-
-
 app.get("/", (req, res) => {
-res.redirect(`/${uuidV4()}`);
+  res.redirect(`/${uuidV4()}`);
 });
 
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
-server.listen(process.env.PORT || 3000);
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
 
+    //messages
+    socket.on("message", (message) => {
+      io.to(roomId).emit("createMessage", message);
+    });
 
-// app.get("/:room", (req, res) => {
-//   res.render("room", { roomId: req.params.room });
-// });
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
+
+server.listen(process.env.PORT || 2121);
