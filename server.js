5aa99431c0594c202ec691c299852ad6430b6fed
Hena-yaris <meraey21@gmail.com>
2024-02-20 05:15:03 -0800

    uuid library that generate unique id

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").Server(app);
+const { v4: uuidV4 } = require("uuid");
+
+
+//permission
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+
+
+
+app.get("/", (req, res) => {
+res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+server.listen(process.env.PORT || 3000);
+
+
+// app.get("/:room", (req, res) => {
+//   res.render("room", { roomId: req.params.room });
+// });
